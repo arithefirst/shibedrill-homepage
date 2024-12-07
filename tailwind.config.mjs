d2e88f7996347ead8635a80f9c0536c66ff880e0
April Hall <ari@arithefirst.com>
2024-12-06 21:11:00 -0500

    feat: Create DaisyUI Theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {}
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#23b0ff',
+          secondary: '#2286c0',
+          accent: '#7692ff',
+          neutral: '#ff00ff',
+          'base-100': '#1d212c',
+          info: '#0000ff',
+          success: '#248232',
+          warning: '#fde74c',
+          error: '#ff3a20',
+
+          '--rounded-box': '0.25rem',
+          '--rounded-btn': '0.25rem',
+          '--rounded-badge': '0.25rem',
+          '--animation-btn': '0.4s',
+          '--animation-input': '0.2s'
+        }
+      }
+    ]
   },
-  plugins: []
+  plugins: [daisyui]
 };
