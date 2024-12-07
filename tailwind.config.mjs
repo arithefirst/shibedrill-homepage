65a558e1173aa926e10cd688bf7c0083b5e2bf72
April Hall <ari@arithefirst.com>
2024-12-07 00:32:22 -0500

    fix: Add formatting to rendered MD
    fix: Add formating and styles to MD rendered through Astro Component
    Collections with @tailwindcss/prose

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui';
+import typography from '@tailwindcss/typography'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -27,5 +28,5 @@ export default {
       }
     ]
   },
-  plugins: [daisyui]
+  plugins: [daisyui, typography]
 };
