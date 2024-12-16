f4e3b4388c464dee06bb001ff6c0b68fdbdecb6a
April Hall <ari@arithefirst.com>
2024-12-16 11:31:00 -0500

    style: Prettier format

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,17 @@ import { getCollection } from 'astro:content';
 import { marked } from 'marked';
 
 export async function GET(context) {
-    const posts = await getCollection('posts');
-    posts.sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date));
-    return rss({
-        title: 'Shibe Drill\'s Website',
-        description: "Posts about programming, infrastructure, Linux, and other nerd shit",
-        site: context.site,
-        items: posts.map((post) => ({
-            title: post.data.title,
-            pubDate: post.data.date,
-            link: `/post/${post.id}`,
-            description: post.body.split('!-- more --')[0].slice(0, -2),
-        })),
-    })
+  const posts = await getCollection('posts');
+  posts.sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date));
+  return rss({
+    title: "Shibe Drill's Website",
+    description: 'Posts about programming, infrastructure, Linux, and other nerd shit',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      link: `/post/${post.id}`,
+      description: post.body.split('!-- more --')[0].slice(0, -2)
+    }))
+  });
 }
