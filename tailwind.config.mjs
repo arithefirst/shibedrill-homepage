bc4378eb07e0b70afafc0297b67348ba12abb6ce
April Hall <ari@arithefirst.com>
2024-12-28 14:46:08 -0500

    feat: Optional font change to Hack

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui';
 import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Hack-Term-Font', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   daisyui: {
     themes: [
       {
